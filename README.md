f4efe87d36eb6206c9ce795dd59f586f4bec4f2c
pgs2285 <wldnjs2284@naver.com>
2024-01-26 14:54:05 +0900

    1991트리순회

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ https://github.com/pgs2285/Algorithm 에서 기초 학습 후, 이어지는 알�
 문제풀이는 대부분 Tistory 개인 블로그에 업로드 해뒀습니다!
 
 
-**(Silver5)2751번 | 수 정렬하기2** | [코드](./2751_Sort/Sort/2751_Sort.cpp) | [설명(Tistory)](https://jisung-it.tistory.com/12)
+**(Silver5)2751번 | 수 정렬하기2** | [코드](./backjoon/2751_Sort/2751_Sort.cpp) | [설명(Tistory)](https://jisung-it.tistory.com/12)
+**(Silver5)1991번 | 트리순회** | [코드](./backjoon/1991_Tree_traversal/1991_Tree_traversal.cpp) | [설명(Tistory)](https://jisung-it.tistory.com/13)
---
+++ backjoon/1991_Tree_traversal/1991_Tree_traversal.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+struct Node
+{
+	string data;
+	Node* left = nullptr;
+	Node* right = nullptr;
+};
+
+void inorder(Node* parent)
+{
+	if (parent == nullptr) return;
+	inorder(parent->left);
+	if (parent->data != ".")cout << parent->data;
+	inorder(parent->right);
+}
+
+void preorder(Node* parent)
+{
+	if (parent == nullptr) return;
+	if (parent->data != ".")cout << parent->data;
+	preorder(parent->left);
+	preorder(parent->right);
+
+}
+
+void postorder(Node* parent)
+{
+	if (parent == nullptr) return;
+	postorder(parent->left);
+	postorder(parent->right);
+	if (parent->data != ".")cout << parent->data;
+
+}
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	unordered_map<string, Node*> nodeList;
+	Node* parent = new Node();
+	
+	int N;
+	std::cin >> N;
+	
+	for (int i = 0; i < N; i++)
+	{
+		string p, r, l;
+		Node* left = new Node();
+		Node* right = new Node();
+		cin >> p >> l >> r;
+		if (i == 0)
+		{
+			parent->data = p;
+			parent->left = left;
+			parent->right = right;
+			nodeList[p] = parent;
+		}
+		left->data = l;
+		right->data = r;
+		nodeList[r] = right;
+		nodeList[l] = left;
+		nodeList[p]->left = left;
+		nodeList[p]->right = right;
+	}
+	preorder(parent);
+	std::cout << "\n";
+	inorder(parent);
+	std::cout << "\n";
+	postorder(parent);
+
+}
+
