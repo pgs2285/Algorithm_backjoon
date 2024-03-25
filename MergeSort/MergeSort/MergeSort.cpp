ff55accb3a452db5ede2466cf81b82097e83b773
pgs2285 <wldnjs2284@naver.com>
2024-03-25 19:02:40 +0900

    2606 바이러스 BFS

---
+++ MergeSort/MergeSort/MergeSort.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+	vector<int> nums(N);
+
+	for (int i = 0; i < N; i++)
+	{
+		int num;
+		cin >> num;
+		nums[i] = num;
+	}
+
+	std::sort(nums.begin(), nums.end());
+	for (int i : nums)
+	{
+		cout << i << endl;
+	}
+}
